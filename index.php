3f7a5b47d6e8777941801c2c7990f152f621a1f0
Anny-Torres <loren01torres@gmail.com>
2025-05-21 15:39:15 -0500

    Subiendo el index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio - Traductor de Lenguaje Esotérico</title>
+</head>
+<body>
+    <div id="saludo-usuario"></div>
+    <div id="mensaje-login"></div>
+    <div id="contenido">
+        <h2>Tus Traducciones</h2>
+    </div>
+    <p><a href="server/user/logout.php">Iniciar sesión usuarios</a></p>
+    <script src="js/app.js"></script>
+    <p><a href="server/user_adm/logout.php">Iniciar sesión administrador</a></p>
+    <script src="js/app_adm.js"></script>
+    
+    
+</body>
+</html>
