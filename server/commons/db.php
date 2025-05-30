5784316a9f849f5c5375ab091d3a3c6c7fb2592f
ronaldarivargas <ronaldlimontco@gmail.com>
2025-05-30 12:08:59 -0500

    Cambios en db.php

--- server/commons/db.php
+++ server/commons/db.php
@@ -1,23 +1,10 @@
 <?php
 
-/*$host = 'localhost';
-$port = '5432';
-$user = 'postgres';
-$pass = '1234';
-$db_name = 'Traductor';*/
-
-
-$host     = getenv('DB_HOST');
-$port     = getenv('DB_PORT') ?: '5432';
-$user     = getenv('DB_USER');
-$pass     = getenv('DB_PASS');
-$db_name  = getenv('DB_NAME');
-
-/*$host     = getenv('DB_HOST') ?: 'localhost';
+$host     = getenv('DB_HOST') ?: 'localhost';
 $port     = getenv('DB_PORT') ?: '5432';
 $user     = getenv('DB_USER') ?: 'postgres';
-$pass     = getenv('DB_PASS') ?: '1234';
-$db_name  = getenv('DB_NAME') ?: 'Traductor';*/
+$pass     = getenv('DB_PASS') ?: '';
+$db_name  = getenv('DB_NAME') ?: 'Traductor';
 
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
